37d6baf43d00c1d2c39ee4b562d8883ed3bb4b5d
Kirill Yegorov <kirill.a.egorov@gmail.com>
2021-08-25 20:57:32 +0300

    CS fix for tests

--- tests/unit/Config/ConfigTest.php
+++ tests/unit/Config/ConfigTest.php
@@ -11,21 +11,21 @@ class ConfigTest extends TestCase
     public function testGet(): void
     {
         $config = new Config('myConfig');
-        $config->setValues(['first'=>1,'second'=>2]);
+        $config->setValues(['first' => 1, 'second' => 2]);
         $this->assertEquals(1, $config->get('first'));
     }
 
     public function testGetInvalid(): void
     {
         $config = new Config('myConfig');
-        $config->setValues(['first'=>1,'second'=>2]);
+        $config->setValues(['first' => 1, 'second' => 2]);
         $this->expectException('InvalidArgumentException');
         $this->assertEquals(1, $config->get('undefined'));
     }
 
     public function testToArray(): void
     {
-        $input = ['first'=>1,'second'=>2];
+        $input = ['first' => 1, 'second' => 2];
         $config = new Config('myConfig');
         $config->setValues($input);
         $this->assertEquals($input, $config->__toArray());
